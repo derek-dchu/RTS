8ae006f7788e17d7a2723f831f300b52908263ef
pxliu <pxliu0830@hotmail.com>
2015-03-31 14:00:14 -0400

    CreditCard class

--- RTS/src/com/mercury/rts/persistence/model/CreditCard.java
+++ RTS/src/com/mercury/rts/persistence/model/CreditCard.java
@@ -1,5 +1,63 @@
 package com.mercury.rts.persistence.model;
 
-public class CreditCard {
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name="rts_credit")
+public class CreditCard implements Serializable{
+	private int cnum;
+	private int cid;
+	private String date;
+	private int userid;
+	
 	public CreditCard() {}
+	public  CreditCard(int cnum, int cid, String date, int userid ){
+		this.cnum=cnum;
+		this.cid=cid;
+		this.date=date;
+		this.userid=userid;
+	}
+
+	@Id
+	@Column(name="cnum")
+	public int getCnum() {
+		return cnum;
+	}
+
+	public void setCnum(int cnum) {
+		this.cnum = cnum;
+	}
+
+	@Column(name="cid")
+	public int getCid() {
+		return cid;
+	}
+
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+
+	@Column(name="date")
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	@Column(name="userid")
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	
+	
 }
