8466d13b345286af5f6c226a2ec5826ffa5ba1ba
Derek Hu <derek.dchu@gmail.com>
2015-04-02 11:18:52 -0400

    create UserDaoImpl

---
+++ RTS/src/com/mercury/rts/persistence/dao/impl/UserDaoImpl.java
@@ -0,0 +1,44 @@
+package com.mercury.rts.persistence.dao.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.mercury.rts.common.db.GenericDaoImpl;
+import com.mercury.rts.persistence.dao.UserDao;
+import com.mercury.rts.persistence.model.User;
+
+@Repository
+public class UserDaoImpl extends GenericDaoImpl<User, Integer> implements UserDao {
+	public UserDaoImpl() {
+		super(User.class);
+	}
+
+	@Override
+	public User getUserById(int id) {
+		return findById(id);
+	}
+
+	@Override
+	public User getUserByEmail(String email) {
+		return findBy("email", email);
+	}
+
+	@Override
+	public User setUserStatus(User user, int status) {
+		user.setEnable(status);
+		saveUser(user);
+		return user;
+	}
+
+	@Override
+	public void saveUser(User user) {
+		save(user);
+	}
+
+	@Override
+	public List<User> listAllUsers() {
+		return findAll();
+	}
+
+}
