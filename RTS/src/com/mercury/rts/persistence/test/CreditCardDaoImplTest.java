d36c2ac35d0dd8d803ee8b0b37ee9e3fe3b3d2a8
Derek Hu <derek.dchu@gmail.com>
2015-04-02 16:54:19 -0400

    create CreditCardDaoImpl test case

---
+++ RTS/src/com/mercury/rts/persistence/test/CreditCardDaoImplTest.java
@@ -0,0 +1,54 @@
+package com.mercury.rts.persistence.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.hibernate.Session;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mercury.rts.persistence.dao.impl.CreditCardDaoImpl;
+import com.mercury.rts.persistence.dao.impl.UserDaoImpl;
+import com.mercury.rts.persistence.model.CreditCard;
+import com.mercury.rts.persistence.model.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "test-persistence.xml")
+@TransactionConfiguration(defaultRollback = false,
+	transactionManager = "transactionManager")
+@Transactional
+public class CreditCardDaoImplTest {
+	@Autowired
+	private CreditCardDaoImpl dao;
+	@Autowired
+	private UserDaoImpl userDao;
+	
+	@Test
+	public void testSaveCreditCard() {
+		
+		CreditCard cc = new CreditCard();
+		cc.setCnum(333);
+		cc.setCid(333);
+		cc.setCdate(3333);
+		
+		User u = userDao.getUserById(1);
+		u.addCreditCard(cc);
+		//dao.saveCreditCard(cc);
+		userDao.saveUser(u);
+	}
+
+	@Test
+	public void testRemoveCreditCard() {
+		CreditCard cc = new CreditCard();
+		cc.setCnum(333);
+		cc.setCid(333);
+		cc.setCdate(3333);
+		
+		dao.delete(cc);
+	}
+
+}
