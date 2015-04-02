bf6737b109fb11d1e7d3823b96d7135595b009ae
Victor Ye <fbnmvskkei@gmail.com>
2015-04-02 13:33:39 -0400

    Update all DaoImpl

--- RTS/src/com/mercury/rts/persistence/dao/impl/CreditCardDaoImpl.java
+++ RTS/src/com/mercury/rts/persistence/dao/impl/CreditCardDaoImpl.java
@@ -1,9 +1,12 @@
 package com.mercury.rts.persistence.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import com.mercury.rts.common.db.GenericDaoImpl;
 import com.mercury.rts.persistence.dao.CreditCardDao;
 import com.mercury.rts.persistence.model.CreditCard;
 
+@Repository
 public class CreditCardDaoImpl extends GenericDaoImpl<CreditCard,Integer> implements CreditCardDao {
 	
 	public CreditCardDaoImpl() {
