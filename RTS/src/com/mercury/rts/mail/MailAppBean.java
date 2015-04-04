0874f35276ae94d02b54424f5de5567b6721e5a0
Derek Hu <derek.dchu@gmail.com>
2015-04-04 19:46:08 -0400

    create MailAppBean

---
+++ RTS/src/com/mercury/rts/mail/MailAppBean.java
@@ -0,0 +1,43 @@
+package com.mercury.rts.mail;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailParseException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+public class MailAppBean {
+	private JavaMailSender mailSender;
+	private SimpleMailMessage simpleMailMessage;
+	
+	public JavaMailSender getMailSender() {
+		return mailSender;
+	}
+	public void setMailSender(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+	public SimpleMailMessage getSimpleMailMessage() {
+		return simpleMailMessage;
+	}
+	public void setSimpleMailMessage(SimpleMailMessage simpleMailMessage) {
+		this.simpleMailMessage = simpleMailMessage;
+	}
+
+	public void sendMail(String to, String dear, String content) throws MailParseException, MailException {
+		MimeMessage message = mailSender.createMimeMessage();
+		try{
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+			helper.setFrom(simpleMailMessage.getFrom());
+			helper.setTo(to);
+			helper.setSubject(simpleMailMessage.getSubject());
+			helper.setText(String.format(simpleMailMessage.getText(), dear, content));
+		} catch (MessagingException e) {
+			throw new MailParseException(e);
+		}
+		System.out.println("Ready to send mail");
+		mailSender.send(message);
+	}
+}
